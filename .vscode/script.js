c327349d212c53840b9306c93f44821e5d83df19
Alme1da04 <joaoaansaloni@gmail.com>
2025-02-06 22:04:31 -0300

    initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  //pegar a tag img
+  const image = document.querySelector("#profile img")
+  //se tiver light mode colocar a imagem light
+  if (html.classList.contains("light")) {
+    image.setAttribute("src", "./assets/Avatar-mayke-light.png")
+  } else {
+    image.setAttribute("src", "./assets/Avatar.png")
+  } //se tiver sem liht mode manter a imagem dark
+}
